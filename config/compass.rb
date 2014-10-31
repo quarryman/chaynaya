562ed2b7f7d7f837ea1e046b85cc3621353999b3
lelik <seomasterweb@gmail.com>
2014-10-31 12:02:20 +0200

    before changinf content width

--- config/compass.rb
+++ config/compass.rb
@@ -3,6 +3,7 @@ css_dir = "app/assets/stylesheets/"
 sass_dir = "app/assets/scss/"
 images_dir = "app/assets/images/"
 javascripts_dir = "app/assets/javascripts/"
+fonts_dir = "app/assets/fonts"
 
 output_style = :expanded
 relative_assets = true

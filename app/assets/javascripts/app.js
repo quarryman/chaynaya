fb7785dad83b6b1107b99dc41acd725e8649baf7
lelik <seomasterweb@gmail.com>
2014-11-02 20:16:32 +0200

    release 2

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,7 +2,7 @@ $(function(){
     var parallaxConfig = [
         {pos : 2000, ratio: 0},
         {pos : 500, ratio: 0.2},
-        {pos : 2500, ratio: 0.25},
+        {pos : 2000, ratio: 0.25},
         {pos : 0, ratio: 0},
         {pos : 600, ratio: .8},
         {pos : 0, ratio: 0}

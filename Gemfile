472b0c57d859a0c51378d8582e45710531db08b9
lelik <seomasterweb@gmail.com>
2014-11-06 15:57:51 +0200

    release 3

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'uglifier', '>= 1.3.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+gem 'jquery-ui-rails'
 
 #assets compilations requires this gem
 gem 'minitest'
--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -31,4 +31,134 @@ $(function(){
             getParallaxPosition()
         });
     }
+
+
+
+
+    //slider
+    ; (function ($, undefined) {
+        $.widget("jaftea.slideshow", {
+            _create: function () {
+                var self = this;
+                this.container = $(".banner-slide", this.element);
+                this.parent = $("#slider");
+                var children = this.container.children("li");
+                this.elementWidth = children.width();
+                this.elementsCount = children.length;
+                this.container.css({ width: (children.length * this.elementWidth) + "px" });
+                this.timer = null;
+
+                $(".slider-nav-link", self.parent).eq(0).addClass("active");
+
+                $.each($(".slider-nav-link", self.parent), function (index) {
+                    $(this).data("index", index);
+                });
+
+                $.each(children, function (index) {
+                    $(this).data("id", index);
+                });
+
+                this.animationIndex = 0;
+
+                //this.autoSlide();
+
+                this.element.on("mouseenter", ".banner", function () {
+                    self.stop();
+                });
+
+                this.element.on("mouseleave", ".banner", function () {
+                    self.autoSlide();
+                });
+
+                this.parent.on("click", ".slider-nav-link", function () {
+                    self.animate($(this).data("index"));
+                });
+                this.parent.on("click", ".slider-nav-left", function () {
+                    var slide = self.previous_slide(self.animationIndex);
+                    self.animate(slide);
+                });
+                this.parent.on("click", ".slider-nav-right", function () {
+                    var slide = self.next_slide(self.animationIndex);
+                    self.animate(slide);
+                    children.removeClass('active');
+                    children.eq(slide).addClass('active');
+                });
+            },
+
+            destroy: function () {
+                $.Widget.prototype.destroy.apply(this, arguments);
+            },
+            next_slide: function (index) {
+
+                if (index == this.elementsCount -1)
+                    return  0;
+                else
+                    return  index + 1;
+            },
+            previous_slide: function (index) {
+
+                if (index == 0)
+                    return  this.elementsCount -1;
+                else
+                    return  index - 1;
+            },
+            animate: function (index) {
+                var self = this;
+                self.animationIndex = index;
+                this.stop();
+                this.container.stop();
+
+                var children = this.container.children("li");
+                var i = 0;
+                var el = this.container.children("li").filter(function (_index) {
+                    if ($(this).data("id") == index) {
+                        i = _index;
+                        return true;
+                    }
+                    return false;
+                });
+
+                $(".slider-nav-link.active", self.element).removeClass("active");
+                $(".slider-nav-link", self.element).eq(index).addClass("active");
+
+                this.container.animate(
+                    {
+                        left: (-i * self.elementWidth) + "px"
+                    },
+                    {
+                        duration: 500,
+                        queue: false,
+                        complete: function () {
+                            var moveEls = self.container.children("li").filter(function (_index) {
+                                return _index < i;
+                            });
+
+                            self.container.append(moveEls);
+
+                            self.container.css({ left: "0px" });
+
+                            self.autoSlide();
+                        }
+                    }
+                );
+            },
+
+            autoSlide: function () {
+                var self = this;
+                clearInterval(this.timer);
+                this.timer = setInterval(function () {
+                    self.animationIndex = self.animationIndex > self.elementsCount - 2 ? 0 : self.animationIndex + 1;
+                    self.animate(self.animationIndex);
+                }, 6000);
+            },
+
+            stop: function () {
+                var self = this;
+                this.container.clearQueue();
+                //this.container.stop();
+                clearInterval(this.timer);
+            }
+        });
+    })(jQuery);
+    $('.banner-slide-wrapper').slideshow();
 });

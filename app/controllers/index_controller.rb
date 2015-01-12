7a31b7e3b9777baab9525901983576d928ea358a
lelik <seomasterweb@gmail.com>
2015-01-12 22:58:31 +0200

    new layout added

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,12 @@
 class IndexController < ApplicationController
   def index
+    respond_to do |format|
+      format.html {render :layout => 'application'}
+    end
+  end
+  def catalog
+    respond_to do |format|
+      format.html {render :layout => 'inner'}
+    end
   end
 end
